fd0204f9ab02d1f0c1840e7cf0d863c8a4fca464
Rajdeep <rajdeepbiswasndp@gmail.com>
2025-01-06 22:44:19 +0530

    hash password and jwt token

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,8 +7,8 @@ const UserSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
-        // unique: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,31 +2,56 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+var jwt=require('jsonwebtoken');
 
-// Create a User using: POST "/api/auth/" . Doesnt req auth
-router.post('/',
+const JWTsec='hello';
+
+// Create a User using: POST "/api/auth/createuser" . Doesnt req auth
+router.post('/createuser',
     [
-        body('name','Enter a valid name').isLength({min:1}),
-        body('email','Enter a valid email').isEmail(),
-        body('password','Password should contain more than 4 chars').isLength({min:5})
+        body('name', 'Enter a valid name').isLength({ min: 1 }),
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password should contain more than 4 chars').isLength({ min: 5 })
     ],
-    (req, res) => {
-    // console.log(req.body);
-    const errors =validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
-    }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user=>res.json(user))
-    .catch(err=>{
-        console.log(err);
-        res.json({error:'Please enter unique email',
-            msg:err.message
-        })
-    });
-})
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        //check whether same email exists
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ error: "Sorry user with same email exists" })
+            }
+            const salt = await bcrypt.genSalt(10); 
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: secPass,
+            })
+            // .then(user=>res.json(user))
+            // .catch(err=>{
+            //     console.log(err);
+            //     res.json({error:'Please enter unique email',
+            //         msg:err.message
+            //     })
+            // });
+            const data={
+                user:{
+                    id: user.id
+                }
+            }
+            const authtoken=jwt.sign(data,JWTsec);
+            res.json({authtoken});
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send("Some err occured");
+        }
+
+    })
 
 module.exports = router
