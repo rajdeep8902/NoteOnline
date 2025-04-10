c4f88bffd3d1e678541bf9b529f9908c854a52cf
Rajdeep <rajdeepbiswasndp@gmail.com>
2025-04-11 00:41:57 +0530

    usecontext and uselocation

---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useContext, useEffect } from 'react'
+import NoteContext from '../context/notes/NoteContext'
+
+const About = () => {
+  
+  return (
+    <div>
+      This is about
+    </div>
+  )
+}
+
+export default About
